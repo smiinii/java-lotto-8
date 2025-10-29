4b0b9a45907fc7e1302fd0b6229b226576fdedda
smini <sm020320@naver.com>
2025-10-29 21:58:10 +0900

    refactor(lotto): 난수 생성 시 재사용할 수 있도록 로또 번호 개수 상수를 public으로 변경

--- src/main/java/lotto/domain/ticket/Lotto.java
+++ src/main/java/lotto/domain/ticket/Lotto.java
@@ -7,7 +7,7 @@ public class Lotto {
 
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
-    private static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_NUMBER_COUNT = 6;
 
     private final List<Integer> numbers;
 
