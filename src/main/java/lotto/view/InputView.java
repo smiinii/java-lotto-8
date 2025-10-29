2ea2cb57579512b279d9c8f44fbe32862890c2ca
smini <sm020320@naver.com>
2025-10-29 20:29:15 +0900

    fix(inputview): 입력 안내 문구 누락 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 }
