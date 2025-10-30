12fd1760a8b66f557ae05075aac1f4b32facdfa1
smini <sm020320@naver.com>
2025-10-30 20:23:34 +0900

    test(parsewinningnumbertest): 쉼표 기준 분리 검증 테스트 추가

--- src/test/java/lotto/util/ParseWinningNumbersTest.java
+++ src/test/java/lotto/util/ParseWinningNumbersTest.java
@@ -3,6 +3,7 @@ package lotto.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ParseWinningNumbersTest {
@@ -14,5 +15,25 @@ public class ParseWinningNumbersTest {
         assertThatThrownBy(() -> InputParser.parseWinningNumbers(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력이 비어있습니다.");
+    }
+
+    @Test
+    @DisplayName("구분자가 쉼표가 아니면 예외처리")
+    void validateDelimiterTest() {
+        // given
+        String input = "1; 2; 3";
+        // when & then
+        assertThatThrownBy(() -> InputParser.parseWinningNumbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("구분자는");
+    }
+
+    @Test
+    @DisplayName("구분자가 쉼표면 성공")
+    void validateDecimalTest2() {
+        // given
+        String input = "1, 2, 3";
+        // when & then
+        assertThat(input).isEqualTo("1, 2, 3");
     }
 }
