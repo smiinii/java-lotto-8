6161c987ae010cb4ba2a3f2305ad78227cadac11
smini <sm020320@naver.com>
2025-10-30 14:35:40 +0900

    feat(lotto): 로또 개당 가격 상수 추가

--- src/main/java/lotto/domain/ticket/Lotto.java
+++ src/main/java/lotto/domain/ticket/Lotto.java
@@ -8,6 +8,7 @@ public class Lotto {
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
     public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_PRICE = 1000;
 
     private final List<Integer> numbers;
 
