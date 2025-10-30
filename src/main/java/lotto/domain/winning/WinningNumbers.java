05efc14d5da0768556f99742f618524b052fce86
smini <sm020320@naver.com>
2025-10-31 08:52:07 +0900

    feat(winningnumbers): 당첨 금액 번호 범위 검증 기능 추가

--- src/main/java/lotto/domain/winning/WinningNumbers.java
+++ src/main/java/lotto/domain/winning/WinningNumbers.java
@@ -10,6 +10,7 @@ public final class WinningNumbers {
 
     private WinningNumbers(List<Integer> numbers) {
         validateCount(numbers);
+        validateRange(numbers);
         this.winningNumbers = List.copyOf(numbers);
     }
 
@@ -24,6 +25,15 @@ public final class WinningNumbers {
     private void validateCount(List<Integer> numbers) {
         if (numbers.size() != LottoRule.COUNT) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호 개수는 " + LottoRule.COUNT + "개 입니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LottoRule.MIN || number > LottoRule.MAX) {
+                throw new IllegalArgumentException("[ERROR] 당첨 금액 번호의 범위는 " + LottoRule.MIN
+                        + " ~ " + LottoRule.MAX + " 입니다.");
+            }
         }
     }
 
