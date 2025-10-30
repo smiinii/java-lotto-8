352f9b18790077e0c4aac7b88c69debe8e1ac3cf
smini <sm020320@naver.com>
2025-10-30 13:51:21 +0900

    feat(inputparser): 숫자 검증 기능 추가

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -4,13 +4,17 @@ public class InputParser {
 
     private InputParser() {}
 
-    public static int parserMoney(String input) {
+    public static int parseMoney(String input) {
         validateEmpty(input);
-        return Integer.parseInt(input);
+        try {
+            return Integer.parseInt(input.trim());
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자가 아닙니다.");
+        }
     }
 
-    private static void validateEmpty(String number) {
-        if (number.isBlank()) {
+    private static void validateEmpty(String input) {
+        if (input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 입력이 비어있습니다.");
         }
     }
