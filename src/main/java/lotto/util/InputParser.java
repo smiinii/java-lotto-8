20214eb83475fd973238b90f8c00fb233cec387c
smini <sm020320@naver.com>
2025-10-30 20:22:29 +0900

    feat(inputparser): 쉼표 기준 분리 기능 추가

--- src/main/java/lotto/util/InputParser.java
+++ src/main/java/lotto/util/InputParser.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class InputParser {
 
+    public static final String DELIMITER = ",";
     private InputParser() {}
 
     public static int parseMoney(String input) {
@@ -20,12 +22,22 @@ public class InputParser {
 
     public static List<Integer> parseWinningNumbers(String input) {
         validateEmpty(input);
-        return new ArrayList<>();
+        validateDelimiter(input);
+        List<String> winningNumbers = List.of(input.split(DELIMITER));
+        return winningNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 
     private static void validateEmpty(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 입력이 비어있습니다.");
+        }
+    }
+
+    private static void validateDelimiter(String input) {
+        Pattern pattern = Pattern.compile(DELIMITER);
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("[ERROR] 구분자는 \"" + DELIMITER + "\"만 사용 가능합니다.");
         }
     }
 }
