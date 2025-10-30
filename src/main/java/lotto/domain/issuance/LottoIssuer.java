4607a05bd33576991e066e95bcf7d73cb3907c32
smini <sm020320@naver.com>
2025-10-31 08:52:14 +0900

    test(winningnumbers): 당첨 금액 번호 범위 검증 테스트 추가

---
+++ src/main/java/lotto/domain/issuance/LottoIssuer.java
@@ -0,0 +1,4 @@
+package lotto.domain.issuance;
+
+public class LottoIssuer {
+}
--- src/test/java/lotto/domain/winning/WinningNumbersTest.java
+++ src/test/java/lotto/domain/winning/WinningNumbersTest.java
@@ -12,10 +12,23 @@ public class WinningNumbersTest {
 
     @Test
     @DisplayName("개수가 6개가 아니면 예외처리")
-    void validateCount() {
-        // given & when & then
-        assertThatThrownBy(() -> WinningNumbers.from(List.of(1, 2, 3, 4, 5, 6, 7)))
+    void validateCountTest() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7);
+        // when & then
+        assertThatThrownBy(() -> WinningNumbers.from(numbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("당첨 번호 개수는");
+    }
+
+    @Test
+    @DisplayName("로또 번호 범위를 넘어가면 예외처리")
+    void validateRangeTest() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 46);
+        // when & then
+        assertThatThrownBy(() -> WinningNumbers.from(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 금액 번호의 범위는");
     }
 }
