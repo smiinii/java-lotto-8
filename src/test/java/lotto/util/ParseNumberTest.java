d8a4dd43a1f2bf5869440339c135493484312e05
smini <sm020320@naver.com>
2025-10-31 09:20:26 +0900

    test(parseNumberTest): 메서드 병합으로 인한 테스트명 변경

--- src/test/java/lotto/util/ParseNumberTest.java
+++ src/test/java/lotto/util/ParseNumberTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ParseMoneyTest {
+public class ParseNumberTest {
 
     @Test
     @DisplayName("양수 입력 시 파싱 성공")
@@ -14,7 +14,7 @@ public class ParseMoneyTest {
         // given
         String input = "12";
         // when
-        int parserInput = InputParser.parseMoney(input);
+        int parserInput = InputParser.parseNumber(input);
         // then
         assertThat(parserInput).isEqualTo(12);
     }
@@ -25,29 +25,29 @@ public class ParseMoneyTest {
         // given
         String input = "-12";
         // when
-        int parserInput = InputParser.parseMoney(input);
+        int parserInput = InputParser.parseNumber(input);
         // then
         assertThat(parserInput).isEqualTo(-12);
     }
 
     @Test
-    @DisplayName("구입 금액이 빈값이면 예외처리")
+    @DisplayName("빈값이면 예외처리")
     void parseMoneyEmptyTest() {
         // given
         String input = "";
         // when & then
-        assertThatThrownBy(() -> InputParser.parseMoney(input))
+        assertThatThrownBy(() -> InputParser.parseNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력이 비어있습니다.");
     }
 
     @Test
-    @DisplayName("구입 금액이 숫자가 아니면 예외처리")
+    @DisplayName("숫자가 아니면 예외처리")
     void parseMoneyValidateDigitTest() {
         // given
         String input = "a";
         // when & then
-        assertThatThrownBy(() -> InputParser.parseMoney(input))
+        assertThatThrownBy(() -> InputParser.parseNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자가 아닙니다.");
     }
