26fb1bd08dedc46f5186d4bd7378eb649f011a94
smini <sm020320@naver.com>
2025-10-31 12:10:43 +0900

    feat(bonusnumber): 당첨 번호와 중복 검증 기능 추가

--- src/main/java/lotto/domain/winning/BonusNumber.java
+++ src/main/java/lotto/domain/winning/BonusNumber.java
@@ -2,17 +2,20 @@ package lotto.domain.winning;
 
 import lotto.domain.rule.LottoRule;
 
+import java.util.List;
+
 public class BonusNumber {
 
     private final int bonusNumber;
 
-    private BonusNumber(int number) {
+    private BonusNumber(int number, List<Integer> winningNumbers) {
         validateRange(number);
+        validateDuplicationWinningNumbers(number, winningNumbers);
         this.bonusNumber = number;
     }
 
-    public static BonusNumber of(int number) {
-        return new BonusNumber(number);
+    public static BonusNumber of(int number, List<Integer> winningNumbers) {
+        return new BonusNumber(number, winningNumbers);
     }
 
     public int getBonusNumber() {
@@ -23,6 +26,14 @@ public class BonusNumber {
         if (number < LottoRule.MIN || number > LottoRule.MAX) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 " + LottoRule.MIN
                     + " ~ " + LottoRule.MAX + " 입니다.");
+        }
+    }
+
+    private void validateDuplicationWinningNumbers(int number, List<Integer> winningNumbers) {
+        for (Integer winningNumber : winningNumbers) {
+            if (number == winningNumber) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호와 중복됩니다.");
+            }
         }
     }
 }
