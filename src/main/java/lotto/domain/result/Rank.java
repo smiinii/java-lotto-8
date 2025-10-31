b0b1ae33c680c35a9d9c21f5c368513d92971089
smini <sm020320@naver.com>
2025-10-31 17:50:42 +0900

    feat(rank): 로또 등수 및 상금을 정의하는 Rank enum 추가

--- src/main/java/lotto/domain/result/Rank.java
+++ src/main/java/lotto/domain/result/Rank.java
@@ -1,4 +1,41 @@
 package lotto.domain.result;
 
+import lotto.domain.ticket.Lotto;
+import lotto.domain.winning.BonusNumber;
+import lotto.domain.winning.WinningNumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Rank {
+
+    FIRST(6,false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private final int winningNumberCount;
+    private final boolean isBonusNumber;
+    private final long prizeMoney;
+
+    Rank(int winningNumberCount, boolean isBonusNumber, long prizeMoney) {
+        this.winningNumberCount = winningNumberCount;
+        this.isBonusNumber = isBonusNumber;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static Rank from(int winningNumbersMatchCount, boolean bonusNumberMatchResult) {
+        if (winningNumbersMatchCount == 6) return FIRST;
+        if (winningNumbersMatchCount == 5 && bonusNumberMatchResult) return SECOND;
+        if (winningNumbersMatchCount == 5) return THIRD;
+        if (winningNumbersMatchCount == 4) return FOURTH;
+        if (winningNumbersMatchCount == 3) return FIFTH;
+        return NONE;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
+    }
 }
