ec53e05327c81bf9a64f1eb72c763fed7d3702b7
smini <sm020320@naver.com>
2025-10-31 13:19:35 +0900

    feat(lottoissuer): 로또 구입 금액에 해당하는 만큼 로또를 발행하는 기능 추가

--- src/main/java/lotto/domain/issuance/LottoIssuer.java
+++ src/main/java/lotto/domain/issuance/LottoIssuer.java
@@ -1,4 +1,29 @@
 package lotto.domain.issuance;
 
+import lotto.domain.generator.LottoNumberGenerator;
+import lotto.domain.purchase.LottoPrice;
+import lotto.domain.purchase.Money;
+import lotto.domain.ticket.Lotto;
+import lotto.domain.ticket.Lottos;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoIssuer {
+
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public LottoIssuer(LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    public Lottos issue(Money money) {
+        int count = money.getMoney() / LottoPrice.UNIT;
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = new Lotto(lottoNumberGenerator.getNumbers());
+            lottos.add(lotto);
+        }
+        return new Lottos(lottos);
+    }
 }
