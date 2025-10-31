8c15be10ac3d79622d0d892d9e24b5b9d85be5ac
smini <sm020320@naver.com>
2025-10-31 12:13:36 +0900

    test(bonusnumber): 당첨 번호와 중복 검증 테스트 추가

--- src/test/java/lotto/domain/winning/BonusNumberTest.java
+++ src/test/java/lotto/domain/winning/BonusNumberTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BonusNumberTest {
@@ -15,8 +16,31 @@ public class BonusNumberTest {
         // given
         int numbers = 46;
         // when & then
-        assertThatThrownBy(() -> BonusNumber.of(numbers))
+        assertThatThrownBy(() -> BonusNumber.of(numbers, List.of(1)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("보너스 번호의 범위는");
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 중복되면 예외처리")
+    void validateDuplicationTest() {
+        // given
+        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 1;
+        // when & then
+        assertThatThrownBy(() -> BonusNumber.of(bonusNumber, winningNumbers.getWinningNumbers()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호와 중복됩니다.");
+    }
+
+    @Test
+    @DisplayName("성공")
+    void success() {
+        // given
+        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 4, 5, 6));
+        // when
+        BonusNumber bonusNumber = BonusNumber.of(7, winningNumbers.getWinningNumbers());
+        // then
+        assertThat(bonusNumber.getBonusNumber()).isEqualTo(7);
     }
 }
