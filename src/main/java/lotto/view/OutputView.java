527aa5030e7c4029496deba46ccb4a3e7eb0d99d
smini <sm020320@naver.com>
2025-10-31 22:29:10 +0900

    feat(outputview): 등수별 당첨 개수 및 상금 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
+import lotto.domain.result.Rank;
 import lotto.domain.ticket.Lotto;
 import lotto.domain.ticket.Lottos;
+
+import java.util.Map;
 
 public class OutputView {
 
@@ -10,5 +13,32 @@ public class OutputView {
         for (Lotto lotto : lottos.getLottos()) {
             System.out.println("[" + lotto.getNumbers() + "]");
         }
+    }
+
+    public void printMatchReult(Map<Rank, Integer> matchReult) {
+        printHeader();
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.NONE) {
+                continue;
+            }
+            printRankLine(rank, matchReult.get(rank));
+        }
+    }
+
+    private void printHeader() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    private void printRankLine(Rank rank, int count) {
+        String bonusText = "";
+        if (rank.isBonusNumber()) {
+            bonusText = ", 보너스 볼 일치";
+        }
+        System.out.printf("%d개 일치%s (%,d원) - %d개%n",
+                rank.getWinningNumberCount(),
+                bonusText,
+                rank.getPrizeMoney(),
+                count);
     }
 }
