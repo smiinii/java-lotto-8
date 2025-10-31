b51843ea85d00cfb2f81f712403a013fdad398ca
smini <sm020320@naver.com>
2025-10-31 13:18:53 +0900

    feat(lottos): Lotto 객체들을 담는 일급 컬렉션 구현

--- src/main/java/lotto/domain/ticket/Lottos.java
+++ src/main/java/lotto/domain/ticket/Lottos.java
@@ -1,4 +1,16 @@
 package lotto.domain.ticket;
 
+import java.util.List;
+
 public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
 }
