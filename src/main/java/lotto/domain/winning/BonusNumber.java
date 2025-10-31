b03333f2e665651b56e638d4879cb975ee7dcf91
smini <sm020320@naver.com>
2025-10-31 11:54:27 +0900

    feat(bonusnumber): 보너스 번호 숫자 범위 검증 기능 추가

--- src/main/java/lotto/domain/winning/BonusNumber.java
+++ src/main/java/lotto/domain/winning/BonusNumber.java
@@ -1,4 +1,28 @@
 package lotto.domain.winning;
 
+import lotto.domain.rule.LottoRule;
+
 public class BonusNumber {
+
+    private final int bonusNumber;
+
+    private BonusNumber(int number) {
+        validateRange(number);
+        this.bonusNumber = number;
+    }
+
+    public static BonusNumber of(int number) {
+        return new BonusNumber(number);
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validateRange(int number) {
+        if (number < LottoRule.MIN || number > LottoRule.MAX) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호의 범위는 " + LottoRule.MIN
+                    + " ~ " + LottoRule.MAX + " 입니다.");
+        }
+    }
 }
