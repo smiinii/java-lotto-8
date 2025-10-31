0a56564c4d97f6b0fd00a54407a2e0222ee238b1
smini <sm020320@naver.com>
2025-10-31 13:20:19 +0900

    feat(fixedlottonumbergenerator): 난수 값을 고정하여 테스트를 진행하기 위해 구현

---
+++ src/main/java/lotto/domain/generator/FixedLottoNumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.domain.generator;
+
+import java.util.List;
+
+public class FixedLottoNumberGenerator implements LottoNumberGenerator {
+
+    private final List<Integer> fixedNumbers;
+
+    public FixedLottoNumberGenerator(List<Integer> fixedNumbers) {
+        this.fixedNumbers = fixedNumbers;
+    }
+
+    @Override
+    public List<Integer> getNumbers() {
+        return fixedNumbers;
+    }
+}
