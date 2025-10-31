892c15732ef25ed3edb5a4439b3cbfedb8ff9837
smini <sm020320@naver.com>
2025-10-31 21:10:03 +0900

    feat(outputview): 발행한 로또 수량 및 번호를 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,14 @@
 package lotto.view;
 
+import lotto.domain.ticket.Lotto;
+import lotto.domain.ticket.Lottos;
+
 public class OutputView {
+
+    public void printIssuedLotto(Lottos lottos) {
+        System.out.println(lottos.getLottoCount() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos.getLottos()) {
+            System.out.println("[" + lotto.getNumbers() + "]");
+        }
+    }
 }
