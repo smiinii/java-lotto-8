8ea2a5157b5b550d4279d28264b1267f40761047
smini <sm020320@naver.com>
2025-10-31 13:20:47 +0900

    test(lottoissuertest): 로또 발행기 작동 검증 테스트 추가

---
+++ src/test/java/lotto/domain/issuance/LottoIssuerTest.java
@@ -0,0 +1,31 @@
+package lotto.domain.issuance;
+
+import lotto.domain.generator.FixedLottoNumberGenerator;
+import lotto.domain.generator.LottoNumberGenerator;
+import lotto.domain.generator.UniqueRandomNumberGenerator;
+import lotto.domain.purchase.Money;
+import lotto.domain.ticket.Lotto;
+import lotto.domain.ticket.Lottos;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoIssuerTest {
+
+    @Test
+    @DisplayName("로또 생성기 테스트")
+    void lottoIssuerTest() {
+        // given
+        LottoNumberGenerator lottoNumberGenerator = new FixedLottoNumberGenerator(List.of(1,2,3,4,5,6));
+        LottoIssuer lottoIssuer = new LottoIssuer(lottoNumberGenerator);
+        Money money = Money.of(3000);
+        // when & then
+        Lottos lottos = lottoIssuer.issue(money);
+        for (Lotto lotto : lottos.getLottos()) {
+            assertThat(lotto.getNumbers()).containsExactly(1,2,3,4,5,6);
+        }
+    }
+}
