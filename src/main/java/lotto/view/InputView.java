c4093c65fee16c219df1e527f5c83f27857efe9e
smini <sm020320@naver.com>
2025-11-01 12:45:49 +0900

    refactor(inputview): 입력 전 줄바꿈 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import javax.crypto.spec.PSource;
+
 public class InputView {
 
     public String readPurchaseAmount() {
@@ -10,11 +12,13 @@ public class InputView {
     }
 
     public String readWinningNumbers() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public String readBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
